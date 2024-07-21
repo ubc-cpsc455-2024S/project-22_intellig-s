6c6cfd2d2ffd72edd080d98a144dc2ae3bdaf82c
Nand Patel <nand.pats@gmail.com>
2024-07-20 19:22:42 -0700

    add deploy script written in js

---
+++ scripts/deploy.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+let url = `https://api.render.com/v1/services/${process.env.MY_RENDER_SERVICE_ID}/deploys`;
+let options = {
+  method: "POST",
+  headers: {
+    accept: "application/json",
+    "content-type": "application/json",
+    authorization: `Bearer ${process.env.MY_RENDER_API_KEY}`,
+  },
+};
+
+let result = await fetch(url, options).json();
+
+const deployId = result.id;
+
+url = `https://api.render.com/v1/services/${process.env.MY_RENDER_SERVICE_ID}/deploys/${deployId}`;
+options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    "content-type": "application/json",
+    authorization: `Bearer ${process.env.MY_RENDER_API_KEY}`,
+  },
+};
+
+let res = await (await fetch(url, options)).json();
+console.log(res);
+
+while (res.status === "build_in_progress") {
+  res = await (await fetch(url, options)).json();
+}
+
+if (res.status === "live") return null;
+throw 1;
