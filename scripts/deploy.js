6a917c8054f576baa3d2bd39d725003416e3d978
Nand Patel <nand.pats@gmail.com>
2024-07-20 19:28:37 -0700

    enclose script in function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,35 +1,39 @@
 require("dotenv").config();
 
-let url = `https://api.render.com/v1/services/${process.env.MY_RENDER_SERVICE_ID}/deploys`;
-let options = {
-  method: "POST",
-  headers: {
-    accept: "application/json",
-    "content-type": "application/json",
-    authorization: `Bearer ${process.env.MY_RENDER_API_KEY}`,
-  },
-};
+async function deploy() {
+  let url = `https://api.render.com/v1/services/${process.env.MY_RENDER_SERVICE_ID}/deploys`;
+  let options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      authorization: `Bearer ${process.env.MY_RENDER_API_KEY}`,
+    },
+  };
 
-let result = await fetch(url, options).json();
+  let result = await fetch(url, options).json();
 
-const deployId = result.id;
+  const deployId = result.id;
 
-url = `https://api.render.com/v1/services/${process.env.MY_RENDER_SERVICE_ID}/deploys/${deployId}`;
-options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    "content-type": "application/json",
-    authorization: `Bearer ${process.env.MY_RENDER_API_KEY}`,
-  },
-};
+  url = `https://api.render.com/v1/services/${process.env.MY_RENDER_SERVICE_ID}/deploys/${deployId}`;
+  options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      authorization: `Bearer ${process.env.MY_RENDER_API_KEY}`,
+    },
+  };
 
-let res = await (await fetch(url, options)).json();
-console.log(res);
+  let res = await (await fetch(url, options)).json();
+  console.log(res);
 
-while (res.status === "build_in_progress") {
-  res = await (await fetch(url, options)).json();
+  while (res.status === "build_in_progress") {
+    res = await (await fetch(url, options)).json();
+  }
+
+  if (res.status === "live") return null;
+  throw 1;
 }
 
-if (res.status === "live") return null;
-throw 1;
+deploy();
