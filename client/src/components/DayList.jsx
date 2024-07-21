4b1763383a5a0d1a5521fa927435e89d16895a43
Nand Patel <nand.pats@gmail.com>
2024-07-21 14:32:28 -0700

    abstract daylist

---
+++ client/src/components/DayList.jsx
@@ -0,0 +1,79 @@
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
+import React from "react";
+import DayCard from "./DayCard";
+
+import PropTypes from "prop-types";
+
+function reorder(list, startIndex, endIndex) {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+}
+
+export default function DayList({ days, setActiveDay, setState }) {
+  const DayList = React.memo(function DayList({ days }) {
+    return days.map((day, index) => (
+      <Draggable
+        key={day.dayNumber}
+        draggableId={`${day.dayNumber}`}
+        index={index}
+      >
+        {(provided) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <DayCard
+              day={{
+                ...day,
+                date: new Date(day.date),
+              }}
+              setActiveDay={setActiveDay}
+              key={day.dayNumber}
+            />
+          </div>
+        )}
+      </Draggable>
+    ));
+  });
+
+  function onDragEnd(result) {
+    if (!result.destination) {
+      return;
+    }
+
+    if (result.destination.index === result.source.index) {
+      return;
+    }
+
+    const reordered_days = reorder(
+      days,
+      result.source.index,
+      result.destination.index
+    );
+
+    setState({ days: reordered_days });
+  }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="list">
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            <DayList days={days} />
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+}
+
+DayList.propTypes = {
+  days: PropTypes.array,
+  setActiveDay: PropTypes.func,
+  setState: PropTypes.state,
+};
--- client/src/pages/ItineraryDetails.jsx
+++ client/src/pages/ItineraryDetails.jsx
@@ -1,24 +1,15 @@
-/* eslint-disable react/prop-types */
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box, Button, Grid, Typography } from "@mui/material";
 import { APIProvider } from "@vis.gl/react-google-maps";
-import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+import { useDispatch, useSelector } from "react-redux";
 
 import ControlledMap from "../components/ControlledMap";
-import DayCard from "../components/DayCard";
 import DayForm from "../components/DayForm";
-import { useDispatch, useSelector } from "react-redux";
+import DayList from "../components/DayList";
+
 import { fetchDays } from "../redux/daySlice";
 import { getItinerariesAsync } from "../redux/itinerarySlice";
-
-function reorder(list, startIndex, endIndex) {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-}
 
 const ItineraryDetails = () => {
   const { id } = useParams();
@@ -32,55 +23,6 @@ const ItineraryDetails = () => {
   const [addDayFormOpen, setAddDayFormOpen] = useState(false);
 
   const [state, setState] = useState({ days: [] });
-
-  const DayList = React.memo(function DayList({ days }) {
-    return days.map((day, index) => (
-      <Draggable
-        key={day.dayNumber}
-        draggableId={`${day.dayNumber}`}
-        index={index}
-      >
-        {(provided) => (
-          <div
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-          >
-            <DayCard
-              day={{
-                ...day,
-                date: new Date(day.date),
-              }}
-              setActiveDay={setActiveDay}
-              key={day.dayNumber}
-            />
-          </div>
-        )}
-      </Draggable>
-    ));
-  });
-
-  function onBeforeCapture(result) {
-    if (state.days.length === 0) setState({ days: days });
-  }
-
-  function onDragEnd(result) {
-    if (!result.destination) {
-      return;
-    }
-
-    if (result.destination.index === result.source.index) {
-      return;
-    }
-
-    const days2 = reorder(
-      state.days,
-      result.source.index,
-      result.destination.index
-    );
-
-    setState({ days: days2 });
-  }
 
   useEffect(() => {
     const fetchDaysFromDB = async () => {
@@ -113,14 +55,12 @@ const ItineraryDetails = () => {
       <Grid container sx={{ height: "100vh" }}>
         <Grid item xs={9} sx={{ pt: "64px", width: "74vw", height: "100%" }}>
           <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-            {itinerary ? (
+            {itinerary && (
               <ControlledMap
                 bounds={itinerary.bounds}
                 markers={markers}
                 activeDay={activeDay}
               ></ControlledMap>
-            ) : (
-              <Typography>No map data available</Typography>
             )}
           </APIProvider>
         </Grid>
@@ -136,7 +76,7 @@ const ItineraryDetails = () => {
           }}
         >
           <Grid item xs={12} sx={{ outline: "10px 10px 10px", mb: 1 }}>
-            {itinerary ? (
+            {itinerary && (
               <Box sx={{ mb: 1 }}>
                 <Typography variant="h4" sx={{ fontWeight: "500" }}>
                   {itinerary.location}
@@ -147,10 +87,6 @@ const ItineraryDetails = () => {
                   {new Date(itinerary.endDate).toLocaleDateString()}
                 </Typography>
               </Box>
-            ) : (
-              <Typography variant="h4" sx={{ fontWeight: "500", mb: "0.25em" }}>
-                {id}
-              </Typography>
             )}
 
             <Button
@@ -169,23 +105,11 @@ const ItineraryDetails = () => {
             </Button>
           </Grid>
           <Grid item xs={12}>
-            <DragDropContext
-              onDragEnd={onDragEnd}
-              onBeforeCapture={onBeforeCapture}
-            >
-              <Droppable droppableId="list">
-                {(provided) => (
-                  <div ref={provided.innerRef} {...provided.droppableProps}>
-                    {state.days.length > 0 ? (
-                      <DayList days={state.days} />
-                    ) : (
-                      <DayList days={days} />
-                    )}
-                    {provided.placeholder}
-                  </div>
-                )}
-              </Droppable>
-            </DragDropContext>
+            <DayList
+              days={state.days.length > 0 ? state.days : days}
+              setState={setState}
+              setActiveDay={setActiveDay}
+            />
           </Grid>
           <Grid item xs={4}>
             <DayForm
