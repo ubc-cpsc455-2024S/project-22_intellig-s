0a066cf4d9fcc71306738630a9a57f51805d407b
Nand Patel <nand.pats@gmail.com>
2024-07-24 20:27:48 -0700

    fix positioning of myitineraries page

--- client/src/pages/MyItineraries.jsx
+++ client/src/pages/MyItineraries.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Container, Typography, Grid, Button } from "@mui/material";
+import { Typography, Grid, Button, Box } from "@mui/material";
 import ItineraryCard from "../components/ItineraryCard";
 import SurveyForm from "../components/SurveyForm";
 import { useSelector, useDispatch } from "react-redux";
@@ -18,37 +18,39 @@ const MyItineraries = () => {
   }, [dispatch]);
 
   return (
-    <Container
-      style={{
-        marginTop: "80px",
-        minHeight: "100vh",
-      }}
-    >
-      <Typography variant="h4" color="primary" gutterBottom>
-        My Itineraries
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => setFormOpen(true)}
-        sx={{
-          mb: 2,
-        }}
-      >
-        Add Itinerary
-      </Button>
-      <Grid container spacing={4}>
-        {itineraries.map((itinerary) => (
-          <Grid item xs={12} sm={6} md={4} key={itinerary.id}>
-            <ItineraryCard itinerary={itinerary} />
+    <Box position={"absolute"} sx={{ top: 0, left: 0, height: "100vh" }}>
+      <Box sx={{ pt: "64px", height: "100%" }}>
+        <Box sx={{ height: "100%", overflow: "auto", px: "20%", pt: 2 }}>
+          <Typography variant="h4" color="primary" gutterBottom>
+            My Itineraries
+          </Typography>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setFormOpen(true)}
+            sx={{
+              mb: 2,
+            }}
+          >
+            Add Itinerary
+          </Button>
+
+          <Grid container spacing={2} sx={{ mb: 10 }}>
+            {itineraries.map((itinerary) => (
+              <Grid item xs={12} sm={6} md={4} key={itinerary.id}>
+                <ItineraryCard itinerary={itinerary} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-      <SurveyForm open={formOpen} handleClose={() => setFormOpen(false)} />
-      <LoadingDialog isOpen={itineraryStatus === "generating"}>
-        Generating...
-      </LoadingDialog>
-    </Container>
+
+          <SurveyForm open={formOpen} handleClose={() => setFormOpen(false)} />
+          <LoadingDialog isOpen={itineraryStatus === "generating"}>
+            Generating...
+          </LoadingDialog>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
