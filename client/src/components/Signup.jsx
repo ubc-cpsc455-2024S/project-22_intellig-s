1bec03195477d568b9ae7ec26b0925241de943d8
Nand Patel <nand.pats@gmail.com>
2024-07-25 18:54:22 -0700

    Login/Signup MUI Components (#59)
    * changes login/signup to only use mui components
    
    * add token expiration check during reload

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,43 +1,72 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { signup } from "../redux/authSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
   const dispatch = useDispatch();
-
-  const { isLoading, error } = useSelector((state) => state.auth);
   const navigate = useNavigate();
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    const username = e.target.username.value;
-    const password = e.target.password.value;
+  const { user, isLoading, error } = useSelector((state) => state.auth);
 
-    dispatch(signup({ username: username, password: password }));
-    if (error) {
-      console.error("Error logging in", error);
-    } else {
-      navigate("/login");
-    }
+  if (user) navigate("/");
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    dispatch(signup({ username, password }));
   };
 
   return (
-    <div>
-      <h2>Sign-up</h2>
-      <form onSubmit={onSubmit}>
-        <input type="text" name="username" placeholder="Username" required />
-        <input
+    <Box
+      sx={{
+        position: "absolute",
+        height: "100vh",
+        width: "100vw",
+        pt: "64px",
+      }}
+    >
+      <Box
+        sx={{
+          pt: 2,
+          position: "relative",
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "330px",
+        }}
+      >
+        <Typography variant={"h4"}>Sign-up</Typography>
+        <Typography sx={{ height: "2em" }} color={"red"}>
+          {error && error.message}
+        </Typography>
+        <TextField
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+          fullWidth
+          sx={{ mb: 1 }}
+        />
+        <TextField
           type="password"
           name="password"
           placeholder="Password"
-          required
+          onChange={(e) => setPassword(e.target.value)}
+          fullWidth
+          sx={{ mb: 1 }}
         />
-        <button type="submit" disabled={isLoading}>
-          signup
-        </button>
-      </form>
-    </div>
+        <Button
+          variant={"contained"}
+          onClick={onSubmit}
+          disabled={isLoading}
+          sx={{ mb: 1 }}
+        >
+          Sign Up
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
