2603c26acd7c66cc9cef79db4da55bb496e3436a
Nand Patel <nand.pats@gmail.com>
2024-08-06 00:05:37 -0700

    bug fix

--- server/utils/cron.js
+++ server/utils/cron.js
@@ -3,13 +3,11 @@ require("dotenv").config();
 
 const keepRenderAlive = new CronJob("*/10 * * * *", async () => {
   try {
-    const response = await fetch(`${process.env.BACKEND_URL}/`, (res) => {
-      res.statusCode === 200 ? console.log("here 1") : console.log("here 2");
+    const response = await fetch(`${process.env.BACKEND_URL}/`);
 
-      response.status === 200
-        ? console.log("server healthy")
-        : console.log("server not healthy");
-    });
+    response.status === 200
+      ? console.log("server healthy")
+      : console.log("server not healthy");
   } catch (e) {
     console.log(`error running script: ${e.message}`);
   }
