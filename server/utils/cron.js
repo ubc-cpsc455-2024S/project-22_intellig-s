b2478e8527b22e365c918c65f1b5fbf209eb1481
Nand Patel <nand.pats@gmail.com>
2024-08-05 23:41:10 -0700

    surround in try catch block

--- server/utils/cron.js
+++ server/utils/cron.js
@@ -2,13 +2,17 @@ const { CronJob } = require("cron");
 require("dotenv").config();
 
 const keepRenderAlive = new CronJob("*/10 * * * *", async () => {
-  const response = await fetch(`${process.env.BACKEND_URL}/`, (res) => {
-    res.statusCode === 200 ? console.log("here 1") : console.log("here 2");
-  });
+  try {
+    const response = await fetch(`${process.env.BACKEND_URL}/`, (res) => {
+      res.statusCode === 200 ? console.log("here 1") : console.log("here 2");
 
-  response.status === 200
-    ? console.log("server healthy")
-    : console.log("server not healthy");
+      response.status === 200
+        ? console.log("server healthy")
+        : console.log("server not healthy");
+    });
+  } catch (e) {
+    console.log(`error running script: ${e.message}`);
+  }
 });
 
 module.exports = { keepRenderAlive };
